ab9e073f61dc3a011f985488473de21c7fb08c4c
fahmiromadon <37509531+fahmiromadon@users.noreply.github.com>
2018-06-07 23:49:50 +0700

    Update main.cpp

--- Modul 10/tgs1/main.cpp
+++ Modul 10/tgs1/main.cpp
@@ -156,6 +156,13 @@ void display(void)
     glPointSize(10);
     glRotated(d,x2,y2,z2);
 
+GLfloat mat_specular[] = { 0.7, 0.7, 0.7, 1.0 };
+GLfloat mat_shininess[] = { 10.0 };
+GLfloat mat_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
+glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+
     glPushMatrix();
     glTranslated(-7.0,-18.0,-9.1);
     glScaled(7.0,7.0,0);
@@ -176,12 +183,6 @@ void display(void)
     glPopMatrix();
 
     glPushMatrix();
-GLfloat mat_specular[] = { 0.7, 0.7, 0.7, 1.0 };
-GLfloat mat_shininess[] = { 10.0 };
-GLfloat mat_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
-glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glColor3f(1,0.5,0);
     glBegin(GL_POLYGON);
     glVertex3d(-9,-15,9.1);
@@ -202,12 +203,6 @@ glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glPopMatrix();
 
     glPushMatrix();
-GLfloat mat_specular_1[] = { 1.7, 0.7, 0.7, 1.0 };
-GLfloat mat_shininess_1[] = { 10.0 };
-GLfloat mat_diffuse_1[] = { 0.7, 0.7, 0.7, 2.0 };
-glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular_1);
-glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess_1);
-glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse_1);
     glTranslated(0,-6,0);
     kubus(18);
     glPopMatrix();
@@ -309,15 +304,47 @@ void kunci(unsigned char key, int x, int y)
         glutPostRedisplay();
         break;
     }
+    if (key=='1'){
+if (amb == true){
+GLfloat ambient_light[] = { 0.3, 0.3, 0.3, 1.0 };
+glLightModelfv(GL_LIGHT_MODEL_AMBIENT,ambient_light);
+amb = false;
+}
+else{
+GLfloat ambient_light[] = { 0.9, 0.9, 0.9, 1.0 };
+glLightModelfv(GL_LIGHT_MODEL_AMBIENT,ambient_light);
+amb = true;
+}
+}
+if (key=='2'){
+if (diff == true){
+glDisable(GL_LIGHT0);
+diff = false;
+}
+else{
+glEnable(GL_LIGHT0);
+diff = true;
+}
+}
+if (key=='3'){
+if (spec == true){
+glDisable(GL_LIGHT1);
+spec = false;
+}
+else{
+glEnable(GL_LIGHT1);
+spec = true;
+}
+}
+display();
 }
 
 void init(void)
 {
     glClearColor (0.0, 0.0, 0.0, 0.0);
-    glColor3f(1.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
+    glColor3f(0.0, 0.0, 0.0);
     glOrtho(-25.0,25.0,-25.0,25.0,-25.0,25.0);
-GLfloat light_position_diff[] = { -1.0, 1.0, 1.0, 1.0 };
+GLfloat light_position_diff[] = { -1.0, 1.0, 1.0, 0.0 };
 GLfloat diffuse_light[] = { 0.0, 0.0, 1.0, 1.0 };
 GLfloat light_position_spec[] = { 1.0, 1.0, 1.0, 0.0 };
 GLfloat specular_light[] = { 0.0, 1.0, 0.0, 1.0 };
